4a02841760c80c2dbd74d5985b44aa3cdc1a5340
rtrikha <raunakstrikha@gmail.com>
2021-02-05 16:50:36 +0530

    adding home page changes

--- js/script.js
+++ js/script.js
@@ -35,3 +35,30 @@ $(function () {
 });
 
 document.getElementById('fluid').play();
+
+// var timer;
+// $(document).mousemove(function () {
+// 	if (timer) {
+// 		clearTimeout(timer);
+// 		timer = 0;
+// 	}
+
+// 	$('div.right, div.footer').fadeIn();
+// 	timer = setTimeout(function () {
+// 		$('div.right, div.footer').fadeOut();
+// 	}, 3000);
+// });
+
+$('.body').mousemove(function (event) {
+	var eye = $('.eye');
+	var x = eye.offset().left + eye.width() / 2;
+	var y = eye.offset().top + eye.height() / 2;
+	var rad = Math.atan2(event.pageX - x, event.pageY - y);
+	var rot = rad * (180 / Math.PI) * -1 + 180;
+	eye.css({
+		'-webkit-transform': 'rotate(' + rot + 'deg)',
+		'-moz-transform': 'rotate(' + rot + 'deg)',
+		'-ms-transform': 'rotate(' + rot + 'deg)',
+		transform: 'rotate(' + rot + 'deg)',
+	});
+});

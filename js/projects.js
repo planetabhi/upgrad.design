ba9890d02e891c7824022b472c5c55f44a19af62
rtrikha <raunakstrikha@gmail.com>
2021-02-12 12:41:29 +0530

    adding new program page design

--- js/projects.js
+++ js/projects.js
@@ -1,13 +1,63 @@
-const colorThief = new ColorThief();
-const img = document.querySelector('img');
+ScrollReveal().reveal('.card-1', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 1500,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	afterReveal: function cleanAnimation() {
+		ScrollReveal().clean('card-2');
+	},
+});
 
-// Make sure image is finished loading
-if (img.complete) {
-	colorThief.getColor(img);
-} else {
-	image.addEventListener('load', function () {
-		colorThief.getColor(img);
-	});
-}
+ScrollReveal().reveal('.card-2', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 1700,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	afterReveal: function cleanAnimation() {
+		ScrollReveal().clean('card-2');
+	},
+});
 
-console.log(colorThief.getColor(img));
+ScrollReveal().reveal('.card-3', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 0,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
+ScrollReveal().reveal('.card-4', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 200,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
+ScrollReveal().reveal('.card-5', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 0,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
+ScrollReveal().reveal('.card-6', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 200,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});

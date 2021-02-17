9a86dd82ee41825eab2120f04faca771a81bc6ed
rtrikha <raunakstrikha@gmail.com>
2021-02-17 16:44:00 +0530

    adding all project cards

--- js/projects.js
+++ js/projects.js
@@ -62,6 +62,36 @@ ScrollReveal().reveal('.card-6', {
 	viewFactor: 0.3,
 });
 
+ScrollReveal().reveal('.card-7', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 0,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
+ScrollReveal().reveal('.card-8', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 200,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
+ScrollReveal().reveal('.card-9', {
+	easing: 'cubic-bezier(0.65, 0, 0.35, 1)',
+	distance: '8px',
+	delay: 0,
+	scale: 0.98,
+	duration: 400,
+	distance: '40px',
+	viewFactor: 0.3,
+});
+
 $(document).on('scroll', function () {
 	if ($(this).scrollTop() >= $('#project-cards').position().top) {
 		document.getElementById('header').style.background = '#fff';
@@ -78,6 +108,11 @@ $(document).on('scroll', function () {
 
 function overlayInit() {
 	var e = document.getElementById('overlay');
-	if (e.style.opacity == '0.8') e.style.opacity = '0';
-	else e.style.opacity = '0.8';
+	if (e.style.opacity == '0.8') {
+		e.style.opacity = '0';
+		e.style.display = 'none';
+	} else {
+		e.style.opacity = '0.8';
+		e.style.display = 'block';
+	}
 }
